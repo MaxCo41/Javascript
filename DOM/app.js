f7e9c8f7c6ae38834e1d954e283c9741d758e681
Maxime Collin <maxime.collin41@gmail.com>
2018-01-15 15:07:51 +0100

    dom done

--- DOM/app.js
+++ DOM/app.js
@@ -47,3 +47,25 @@ firstH3.innerHTML = "<em>Itelic title ! yeah  !<em>";
 
 let firstH2 = document.querySelector ("h2");
 firstH2.innerText = "<strong>HTML doens't work !</strong>";
+
+//Ex4--------------------
+let liste = document.getElementsByTagName("ul")[0];
+let nouveauLi = document.createElement ("li");
+nouveauLi.innerHTML = "Mon meilleur ami est <a href='http://www.google.com'>Google</a>";
+liste.appendChild (nouveauLi);
+let firstLien = nouveauLi.getElementsByTagName("a")[0];
+firstLien.style.color = "red";
+
+//Ex 5-----------------
+let liste2 = document.getElementsByTagName ("ol")[0];
+let childList = liste2.children;
+for (var i = childList.length - 1; i >= 0; i--) {
+  liste2.removeChild(childList[i]);
+};
+
+let codes = ["Silent Teacher","Code Monkey", "CodeCombat"];
+for (var i = 0; i < codes.length; i++) {
+let newLi = document.createElement ("li");
+newLi.innerText = codes[i];
+liste2.appendChild (newLi);
+}
